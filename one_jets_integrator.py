2509a2a5fb141365c031a7aa3436a3bf08f0c6c2
hoj201 <hoj201@gmail.com>
2013-08-31 07:33:22 -0700

    rewrote kernel tensors routine.  Energy behavior still bad unless mu is small

--- one_jets_integrator.py
+++ one_jets_integrator.py
@@ -2,7 +2,7 @@ from numpy import array, concatenate, exp, sqrt, ones, zeros, outer, empty, eins
 from numpy.random import rand, randn
 from numpy.linalg import cond, solve
 from scipy.integrate import odeint
-N_PART = 10
+N_PART = 5
 DIM = 2
 SIGMA = 1./sqrt(N_PART)
 
@@ -19,12 +19,15 @@ def get_kernel_tensors( x ):
 
 	# Generate the Kernel tensors
 	K = exp( - sq_mag / (2*SIGMA**2) )
-	dK = (-1./(SIGMA**2)) * einsum('jab,ab->jab',delta_x,K)
-	d2K = (-1./(SIGMA**2)) * (einsum('jk,ab->jkab', delta , K) + einsum('jab,kab->jkab',delta_x,dK))
-	d3K = (-1./(SIGMA**2))*(einsum('jk,lab->jklab',delta,dK) + einsum('jl,kab->jklab',delta,dK) + einsum('jab,klab->jklab',delta_x,d2K))
-	d4K = (-1./(SIGMA**2))*(einsum('jk,lmab->jklmab',delta,d2K)+einsum('jl,kmab->jklmab',delta,d2K)+einsum('jm,klab->jklmab',delta,d2K)+einsum('jab,klmab->jklmab',delta_x,d3K))
-	d5K = (-1./(SIGMA**2))*(einsum('jk,lmnab->jklmnab',delta,d3K)+einsum('jl,kmnab->jklmnab',delta,d3K)+einsum('jm,klnab->jklmnab',delta,d3K)+einsum('jn,klmab->jklmnab',delta,d3K) + einsum('jab,klmnab->jklmnab',delta_x,d4K))
-	return K,dK,d2K,d3K,d4K,d5K
+	dK = (-1./(SIGMA**2)) * einsum('aij,ij->aij',delta_x,K)
+	term1 = einsum('ab,ij->abij',delta,K)
+	term2 = einsum('aij,bij->abij',delta_x, dK)
+	d2K = (-1./SIGMA**2) *( term1 + term2)
+	term1 = einsum('ab,cij->abcij',delta,dK)
+	term2 = einsum('ac,bij->abcij',delta,dK)
+	term3 = einsum('aij,bcij->abcij',delta_x,d2K)
+	d3K = (-1./SIGMA**2) * (term1 + term2 + term3 )
+	return K,dK,d2K,d3K
 
 def state_decomp( state ):
 	q = empty( [DIM , N_PART] )
@@ -42,7 +45,7 @@ def state_decomp( state ):
 	return q,p,mu
 
 def state_reconstruction( q , p , mu ):
-	state = empty( [DIM*N_PART + DIM*N_PART + DIM*DIM*N_PART ] )
+	state = empty( DIM*N_PART + DIM*N_PART + DIM*DIM*N_PART )
 	begin = 0
 	end = DIM*N_PART
 	state[begin:end] = q.reshape( DIM*N_PART )
@@ -57,15 +60,15 @@ def state_reconstruction( q , p , mu ):
 def get_energy( state ):
 	#something is wrong with this energy function
 	q,p,mu = state_decomp( state )
-	K,dK,d2K,d3K,d4K,d5K = get_kernel_tensors(q)
-	term = einsum('ai,aj,ij',p,p,K)
+	K,dK,d2K,d3K = get_kernel_tensors(q)
+	term = 1*einsum('ai,aj,ij',p,p,K)
 	term = term + 2* einsum('abi,aj,bij',mu,p,dK)
-	term = term - einsum('abi,agj,bgij',mu,mu,d2K)
+	term = term - 1* einsum('abi,agj,bgij',mu,mu,d2K)
 	return 0.5*term 
 
 def get_state_velocity( state , t ):
 	q,p,mu = state_decomp( state )
-	K,dK,d2K,d3K,d4K,d5K = get_kernel_tensors( q )
+	K,dK,d2K,d3K = get_kernel_tensors( q )
 	dq = einsum( 'aj,ij->ai',p,K) + einsum( 'abj,bij',mu,dK )
 	xi = einsum( 'aj,bij->abi',p,dK) - einsum('agj,bgij->abi',mu,d2K)
 	theta = einsum( 'aj,bgij->abgi',p,d2K) - einsum('adj,bgdij->abgi',mu,d3K)
@@ -76,14 +79,16 @@ def get_state_velocity( state , t ):
 
 q = rand(DIM,N_PART)
 p = rand(DIM,N_PART)
-mu = 1.1*rand(DIM,DIM, N_PART)
+mu = 0.01*rand(DIM,DIM, N_PART)
 
 state = state_reconstruction( q , p , mu )
+
 t_end = 100
-time = linspace(0,0.1,t_end)
+time = linspace(0.0,2.0,t_end)
 state_hist = odeint( get_state_velocity , state , time )
 print state_hist.shape
 energy = empty(t_end)
 for t in range(0,t_end):
 	energy[t] = get_energy( state_hist[t] )
 print energy
+
